0a3fd78a1f569a6497e790d885080f809c6045a5
yunjongyun <yunyami0605@gmail.com>
2025-07-29 01:05:02 +0900

    homework: 네이버 책검색 숙제 내용 추가
    4~6번 질문 추가

--- 4_250729_네이버책검색/search_books.py
+++ 4_250729_네이버책검색/search_books.py
@@ -33,17 +33,38 @@ def search_books(query):
 
     os.makedirs('./data', exist_ok=True)
     
-    # 1. 검색한 책 json으로 저장하기
+    # 1. 검색어로  찾은  책 목록을 json 파일로 저장하기
     with open('./data/books.json', 'w', encoding='utf-8') as file:
         # file.write(items_data)
         json.dump(items_data, file, ensure_ascii=False, indent=2)
 
-    # pandas로 csv로 저장
+    # 2.  books.json 파일을 Pandas DataFrame로 저장하기
     df = pd.read_json('./data/books.json')
-
-    # 읽어서 출력하기
     df.to_csv('./data/books.csv', index=False, encoding='utf-8-sig')
-    print(df.head(50))
+
+    # 3. 검색어로  찾은  책 목록 출력하기
+
+    books = pd.DataFrame(items_data)
+    print(books.head(50))
+
+    # discount 컬럼을 숫자로 변환 (에러 방지용)
+    books['discount'] = pd.to_numeric(books['discount'], errors='coerce')
+
+    # 4. 검색어로  찾은  책 목록 중에서 가격이 2만원 이상인 책만 출력하기
+    filtered_books = books.loc[books['discount'] > 20000, ["title", "author", "discount", "publisher", "pubdate"]] \
+                        .sort_values(by='discount', ascending=False) \
+                        .reset_index(drop=True)
+    print(filtered_books)
+    
+    # 6. 질문 :  검색어로  찾은  책 목록 중에서 출판사가 인피니티북스인 책만 출력하기
+    filtered_books2 = books.loc[books['publisher'] == "인피니티북스", [col for col in df.columns if col not in ['image', 'description']] ] \
+                        .reset_index(drop=True)
+    print("@ filtered_books2")
+    print(filtered_books2)
+
+    # 6. 질문 :  검색어로  찾은  책 목록 중에서 출판사가 인피니티북스인 책만 출력하기
+
+
 
 
 search_books("엔비디아")
